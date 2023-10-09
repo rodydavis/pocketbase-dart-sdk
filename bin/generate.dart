75cce9f5a110f31640d9af868712783168a3680c
Rody Davis <rody.davis.jr@gmail.com>
2023-10-09 10:32:06 -0700

    Create generate.dart

---
+++ bin/generate.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  // TODO: Login with admin client
+
+  // TODO: Get models from args (default to all collections)
+
+  // TODO: Add CRDT fields on models (hlc, is_deleted)
+
+  // TODO: Generate classes for models
+
+  // TODO: Generate triggers for relations
+
+  // TODO: Generate precache for AOT bundle
+
+  // TODO: Generate client SDK
+}
